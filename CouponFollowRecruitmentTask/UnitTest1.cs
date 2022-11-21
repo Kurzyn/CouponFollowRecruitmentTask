874f855cf3df2645b652397df33a06a0d45d1ae8
Kacper Kurzyński <kkurzyn@outlook.com>
2022-11-21 18:32:42 +0100

    in-assembly parallelization added

--- CouponFollowRecruitmentTask/UnitTest1.cs
+++ CouponFollowRecruitmentTask/UnitTest1.cs
@@ -1,3 +1,8 @@
+
+//"IAP" parallelization added thru anotation due to lack of support of .runsettings on Mac and in Visual Studio Code
+//https://learn.microsoft.com/en-us/visualstudio/test/configure-unit-tests-by-using-a-dot-runsettings-file?view=vs-2022
+//this feature can potentialy be marked as included only for specific builds
+[assembly: Parallelize(Workers = 4, Scope = ExecutionScope.MethodLevel)]
 namespace CouponFollowRecruitmentTask
 {
     [TestClass]
