d499518f50e69c00a1e888e58c6294d735a6733c
Kacper Kurzyński <kkurzyn@outlook.com>
2022-11-21 19:13:42 +0100

    main page added

---
+++ CouponFollowRecruitmentTask/Pages/MainPage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CouponFollowRecruitmentTask.Pages
+{
+    
+    public class MainPage
+    {
+        private IWebDriver Driver { get; }
+        
+        public MainPage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public void OpenMainPage()
+        {
+            Driver.Navigate().GoToUrl("https://couponfollow.com");
+        }
+    }
+}
