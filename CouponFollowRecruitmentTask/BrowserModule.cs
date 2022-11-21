d41b08489cb9f81f12db496742d43009b65efe10
Kacper Kurzyński <kkurzyn@outlook.com>
2022-11-21 18:54:29 +0100

    basic driver provider added

---
+++ CouponFollowRecruitmentTask/BrowserModule.cs
@@ -0,0 +1,13 @@
+﻿using Ninject.Modules;
+using OpenQA.Selenium;
+
+namespace CouponFollowRecruitmentTask
+{
+    internal class BrowserModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IWebDriver>().ToProvider<WebDriverProvider>().InThreadScope();
+        }
+    }
+}
---
+++ CouponFollowRecruitmentTask/WebDriverProvider.cs
@@ -0,0 +1,14 @@
+﻿using Ninject.Activation;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CouponFollowRecruitmentTask
+{
+    internal class WebDriverProvider : Provider<IWebDriver>
+    {
+        protected override IWebDriver CreateInstance(IContext context)
+        {
+            return new ChromeDriver();
+        }
+    }
+}
